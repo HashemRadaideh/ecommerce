d10e9901a5ac8c37290c854006bbf9b76b26e190
HashemRadaideh <hashemalradaideh@gmail.com>
2024-05-15 18:28:06 +0300

    Added products endpoint

--- api/routes/api/products/index.ts
+++ api/routes/api/products/index.ts
@@ -6,9 +6,10 @@ export const products = express.Router();
 
 products
   .route("/api/products")
-  .post(async (_req: express.Request, res: express.Response) => {
+  .get(async (_req: express.Request, res: express.Response) => {
     try {
-      res.send(await getProducts());
+      const products = await getProducts();
+      res.json(products);
     } catch (error) {
       console.error("Failed to get all products", error);
       res.status(500).json({ error: "Failed to get all products" });
