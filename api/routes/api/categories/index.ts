001388dffc3d97f0c4148ab750ded5eb3c4d0025
HashemRadaideh <hashemalradaideh@gmail.com>
2024-05-15 18:28:25 +0300

    Added categories endpoint

---
+++ api/routes/api/categories/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import { getCategories } from "@/api/models/categories";
+
+export const categories = express.Router();
+
+categories
+  .route("/api/categories")
+  .get(async (_req: express.Request, res: express.Response) => {
+    try {
+      const categories = await getCategories();
+      res.json(categories);
+    } catch (error) {
+      console.error("Failed to get all categories", error);
+      res.status(500).json({ error: "Failed to get all categories" });
+    }
+  });
