b9a1e14449560f63e7b93a048421bf5c36ec0825
HashemRadaideh <hashemalradaideh@gmail.com>
2024-05-16 17:01:55 +0300

    Changed deployment strategy

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,4 +8,4 @@ export function cn(...inputs: ClassValue[]) {
 export const api =
   process.env.NODE_ENV !== "production"
     ? `http://${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}`
-    : "";
+    : `${process.env.NEXT_PUBLIC_DOMAIN}`;
